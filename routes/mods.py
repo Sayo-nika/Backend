aec96f17e16acad4ccaacb814e22acdeabe1d8a9
Martmists <martmists@gmail.com>
2018-10-09 12:40:32 +0200

    Add more routes
    Signed-off-by: Martmists <martmists@gmail.com>

--- routes/mods.py
+++ routes/mods.py
@@ -60,7 +60,7 @@ class Userland(RouteCog):
 
     @route("/api/v1/mods/<mod_name>", methods=["PATCH"])
     @json
-    def get_recent_releases(self, mod_name):
+    def patch_mod(self, mod_name):
         file = request.files.get('file')
 
         if file is None or not file.endswith(".zip"):
@@ -77,12 +77,12 @@ class Userland(RouteCog):
         if mod_name not in [mod["title"] for mod in self.data["mods"]]:
             abort(400, f"The mod '{mod_name}' does not exist.")
 
-        mod["verified"] = False
-        mod["last_updated"] = mod["released_at"] = datetime.utcnow().timestamp()
-        mod["downloads"] = 0
-        mod["path"] = self.new_path()
-
         old_mod = [mod for mod in self.data["mods"] if mod["title"] == mod_name][0]
+
+        mod["verified"] = old_mod["verified"]
+        mod["last_updated"] = datetime.utcnow().timestamp()
+        mod["downloads"] = old_mod["downloads"]
+        mod["path"] = self.new_path()
 
         os.remove(f"mods/{old_mod['path']}.zip")
         file.save(f"mods/{mod['path']}.zip")
@@ -92,6 +92,75 @@ class Userland(RouteCog):
         self.data["update"] = 0
 
         return jsonify(mod)
+
+    @route("/api/v1/mods/<mod_name>/reviews", methods=["POST"])
+    @json
+    def post_review(self, mod_name: str):
+        if mod_name not in [mod["title"] for mod in self.data["mods"]]:
+            abort(400, f"The mod '{mod_name}' does not exist.")
+
+        review = {"mod": mod_name}
+
+        for attribute in ("author", "message"):
+            val = request.form.get(attribute)
+            if val is None:
+                abort(400, f"Missing POST parameter: '{attribute}'.")
+            mod[attribute] = val
+
+        if review["author"] not in [user["name"] for user in self.data["users"]]
+            abort(400, f"A user with name '{review['author']}' does not exist.")
+
+        self.data["reviews"].append(review)
+
+        return jsonify(review)
+
+    @route("/api/v1/users", methods=["POST"])
+    @json
+    def post_users(self):
+        user = {}
+
+        for attribute in ("name", "bio"):
+            val = request.form.get(attribute)
+            if val is None:
+                abort(400, f"Missing POST parameter: '{attribute}'.")
+            user[attribute] = val
+
+        if user["name"] in [user["name"] for user in self.data["users"]]
+            abort(400, f"A user with name '{user['name']}' already exists.")
+
+        user["mods"] = []
+        user["favorites"] = []
+
+        self.data["users"].append(user)
+
+        return jsonify(user)
+
+    @route("/api/v1/users/<user_name>", methods=["patch"])
+    @json
+    def patch_user(self, user_name: str):
+        user = {}
+
+        for attribute in ("name", "bio"):
+            val = request.form.get(attribute)
+            if val is None:
+                abort(400, f"Missing POST parameter: '{attribute}'.")
+            user[attribute] = val
+
+        if user_name not in [user["name"] for user in self.data["users"]]
+            abort(400, f"A user with name '{user['name']}' does not exist.")
+
+
+        old_user = [user for user in self.data["users"]
+                    if user["name"] == user_name][0]
+
+        user["mods"] = old_user["mods"]
+        user["favorites"] = old_user["favorites"]
+
+        self.data["users"] = [user for user in self.data["users"] if not user["name"] == user_name]
+        self.data["users"].append(user)
+        self.data["update"] = 0
+
+        return jsonify(user)
 
 
 def setup(core: Sayonika):
