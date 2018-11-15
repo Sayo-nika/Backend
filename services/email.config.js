b34ca1f05d99ac6acdadef7d56639d98ab345d6c
Capuccino <enra@sayonika.moe>
2018-11-15 20:33:26 +0800

    Fix host being set with a int port

--- services/email.config.js
+++ services/email.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-     host: '' || process.env.SAYONIKA_EMAIL_HOST || 3000,
-     port: '' || process.env.SAYONIKA_EMAIL_PORT || '67',
+     host: '' || process.env.SAYONIKA_EMAIL_HOST,
+     port: '' || process.env.SAYONIKA_EMAIL_PORT || 3000,
      auth: {
         user: '' || process.env.SAYONIKA_EMAIL_USER,
         password: '' || process.env.SAYONIKA_EMAIL_PASSWORD
