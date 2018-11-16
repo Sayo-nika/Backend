52c8b4eea9f4c90d631fbedff3efb3cf306f6a93
Capuccino <chinodesuuu@gmail.com>
2018-11-17 00:47:22 +0800

    Lint fixes

--- services/loader.js
+++ services/loader.js
@@ -3,10 +3,10 @@
  * @description server.js microservice loader
  * @author Capuccino
  */
-const fs = require('fs');
+const fs = require("fs");
 const configRegex = str => /[a-zA-Z]+\.config\.js/gi.test(str);
 
-let files = fs.readdirSync(__dirname).filter(v => v !== 'index.js' && v.endsWith('.js') && !configRegex(v) && v !== 'server.js');
+let files = fs.readdirSync(__dirname).filter(v => v !== "index.js" && v.endsWith(".js") && !configRegex(v) && v !== "server.js");
 
 files.forEach(v => {
     require(`${__dirname}/${v}`);
