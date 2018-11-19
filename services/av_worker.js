0751ac8f0780aabf009688e3abef7e115b601e07
Capuccino <chinodesuuu@gmail.com>
2018-11-19 19:37:50 +0800

    fix bool parsing on vtRes

--- services/av_worker.js
+++ services/av_worker.js
@@ -18,14 +18,14 @@ const server = micro(async (req, res) => {
         let vtRes = await request("POST", `https://www.virustotal.com/vtapi/v2/url/report`, {headers: {"Content-Type": "appplication/json", "Accept-Encoding": "gzip, deflate"}}, JSON.stringify({resource: uwu.url, apikey: config.apiKey, scan: "1"}));
 
         if (vtRes !== "204") send(res, 429, JSON.stringify({code: "429", message: "Exceeded Virustotal ratelimit."}));
-        
+
         else {
 
             // Scans the URL. We inform the front-facing REST if its safe or not.
 
             let data = JSON.parse(vtRes).body;
 
-            if (data.scans.MalwarePatrol.detected !== "false") send(res, 200, JSON.stringify({code: "200", is_bad: true, message: "Reported URL Malware. Delete immediately."}));
+            if (!data.scans.MalwarePatrol.detected) send(res, 200, JSON.stringify({code: "200", is_bad: true, message: "Reported URL Malware. Delete immediately."}));
 
             else send(res, 200, JSON.stringify({code: "200", is_bad: false, message: "URL Reported OK. No Malware found."}));
         }
