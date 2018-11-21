4c11981ec5f2cfda57a15371d23cfd797327ba2e
Capuccino <enra@sayonika.moe>
2018-11-21 17:19:26 +0800

    why is there's a $ in here

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "-------- TODOs: --------"
-grep -i "TODO" $(find -wholename "./*/*.py"  | grep .py$)
+grep -i "TODO" $(find -wholename "./*/*.py"  | grep .py)
 grep -i "TODO" $(find -wholename "./services/*.js" | grep .js)
 echo "------------------------"
 
