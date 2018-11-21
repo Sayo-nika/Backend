ab4ab058bd1d558df593a1d9085b134a7881c255
Capuccino <enra@sayonika.moe>
2018-11-21 17:09:32 +0800

    fix TODO finding logic

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo "-------- TODOs: --------"
-grep -i "TODO" $(find . | grep .py$)
+grep -i "TODO" $(find . -type d \( -path mods -o -path routes \) -prune -o -print | grep .py$)
+grep -i "TODO" $(find ./services/| grep .js$)
 echo "------------------------"
+
