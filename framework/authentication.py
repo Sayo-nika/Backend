7872cfaf67c41ec7b5fc27e530d3d33f351305a0
Capuccino <enra@sayonika.moe>
2018-12-13 15:28:17 +0800

    style: comply to C0120
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- framework/authentication.py
+++ framework/authentication.py
@@ -32,7 +32,7 @@ class Authenticator:
 
         user = User.get_s(id_)
 
-        if user.email_verified != True:
+        if user.email_verified is False:
             abort(401, "Account needs verification")
 
         if user is None:
