861408fa10c5b57e236ec0e330726f253a8f0812
Capuccino <enra@sayonika.moe>
2018-12-13 15:19:53 +0800

    feat: add fixme checker
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ grep -i "TODO" $(find -wholename "./*/*.py"  | grep .py)
 grep -i "TODO" $(find -wholename "./services/*.js" | grep .js)
 echo "------------------------"
 
+echo "-------- FIXMEs: --------"
+grep -i "FIXME" $(find -wholename "./*/*.py"  | grep .py)
+grep -i "FIXME" $(find -wholename "./services/*.js" | grep .js)
+echo "------------------------"
