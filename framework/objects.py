68086fdcfc06f0532d00437b3741cf16e52cca89
Ovyerus <iamovyerus@gmail.com>
2018-12-25 12:59:57 +1100

    feat: service for generating + verifying JWT tokens

--- framework/objects.py
+++ framework/objects.py
@@ -9,9 +9,10 @@ from flask_limiter.util import get_remote_address
 # Sayonika Internals
 from framework.authentication import Authenticator
 from framework.database import DBHandler
+from framework.tokens import JWT
 from framework.sayonika import Sayonika
 
-__all__ = ("sayonika_instance", "limiter", "logger", "auth_service", "database_handle")
+__all__ = ("sayonika_instance", "limiter", "logger", "auth_service", "jwt_service", "database_handle")
 
 sayonika_instance = Sayonika()
 
@@ -21,7 +22,8 @@ SETTINGS = {
     "DB_PORT": 5432,
     "DB_USER": "mart",
     "DB_PASS": "Nya",
-    "DB_NAME": "sayonika"
+    "DB_NAME": "sayonika",
+    "JWT_SECRET": "testing123"
 }
 
 SETTINGS.update({
@@ -45,6 +47,7 @@ logger = logging.getLogger("Sayonika")
 logger.setLevel(logging.INFO)
 
 auth_service = Authenticator(SETTINGS)
+jwt_service = JWT(SETTINGS)
 
 database_handle = DBHandler(user=SETTINGS["DB_USER"], password=SETTINGS["DB_PASS"],
                             database=SETTINGS["DB_NAME"], host=SETTINGS["DB_HOST"],
---
+++ framework/tokens.py
@@ -0,0 +1,43 @@
+# Stdlib
+from datetime import datetime
+
+# External Libraries
+import jwt
+from pony.orm import db_session
+
+# Sayonika Internals
+from framework.models import User
+
+class JWT:
+    algorithm = 'HS256'
+
+    def __init__(self, settings: dict):
+        # `settings` is the dict of all ENV vars starting with SAYONIKA_
+        self.secret = settings["JWT_SECET"]
+
+    def make_token(self, id, password_reset):
+        payload = {
+            "id": id,
+            "lr": password_reset,
+            "iat": datetime.utcnow()
+        }
+        token = jwt.encode(payload, self.secret, algorithm=self.algorithm)
+
+        return token
+
+    @db_session
+    def verify_token(self, token):
+        try:
+            decoded = jwt.decode(token, self.secret, algorithms=self.algorithm)
+        except:
+            return False # Any errors thrown during decoding probably indicate bad token in some way
+
+        if set(decoded.keys()) != set(["id", "lr", "iat"]):
+            return False # Keys should only be the ones we give
+
+        user = User.get_s(decoded["id"])
+
+        if user is None or decoded["lr"] != user.last_pass_reset or decoded["iat"] < datetime.utcnow():
+            return False
+
+        return True
