38899b79d22f38113d945e7da6cb9791fe8a9f8f
Ovyerus <iamovyerus@gmail.com>
2019-01-12 22:01:15 +1100

    fix: change `main` function to a decorator for server to run before start
    Fixes an asyncio error complaining about an already running event loop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Stdlib
 import sys
-import asyncio
 
 # External Libraries
 from sqlalchemy.engine.url import URL
@@ -13,10 +12,10 @@ sayonika_instance.debug = (len(sys.argv) > 1 and
 sayonika_instance.gather("routes")
 
 
+@sayonika_instance.before_serving
 async def main():
     await db.set_bind(URL("asyncpg", username=SETTINGS["DB_NAME"], password=SETTINGS["DB_PASS"],
                           host=SETTINGS["DB_HOST"], port=SETTINGS["DB_PORT"], database=SETTINGS["DB_NAME"]))
     await db.gino.create_all()
-    sayonika_instance.run(SETTINGS["SERVER_BIND"], int(SETTINGS["SERVER_PORT"]))
 
-asyncio.get_event_loop().run_until_complete(main())
+sayonika_instance.run(SETTINGS["SERVER_BIND"], int(SETTINGS["SERVER_PORT"]))
