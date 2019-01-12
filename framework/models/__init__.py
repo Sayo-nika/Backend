24fb61db51aaed2980495e8183a1d670ee81f56f
Ovyerus <iamovyerus@gmail.com>
2019-01-12 11:26:21 +1100

    refactor(models): explicit import + define __all__

--- framework/models/__init__.py
+++ framework/models/__init__.py
@@ -1,8 +1,11 @@
-from base import *
-from connection import *
-from enums import *
-from media import *
-from mod import *
-from report import *
-from review import *
-from user import *
+from base import Base
+from connection import Connection
+from enums import ModStatus, Category, ConnectionType, MediaType
+from media import Media
+from mod import Mod
+from report import Report
+from review import Review, ReviewDownvoters, ReviewHelpfuls, ReviewUpvoters
+from user import User, UserFavorites, UserMods
+
+__all__ = ("Base", "Connection", "ModStatus", "Category", "ConnectionType", "MediaType", "Media", "Mod", "Report",
+           "Review", "ReviewDownvoters", "ReviewHelpfuls", "ReviewUpvoters", "User", "UserFavorites", "UserMods")
