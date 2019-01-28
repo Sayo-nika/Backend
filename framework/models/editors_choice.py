a63beebfd292e3b3d105c5605546e9a310c6aff7
Capuccino <enra@sayonika.moe>
2019-01-28 09:30:17 +0800

    style(models): add newline after _-tablename
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- framework/models/editors_choice.py
+++ framework/models/editors_choice.py
@@ -4,6 +4,7 @@ from .base import Base
 
 class EditorsChoice(db.Model, Base):
     __tablename__ = "editors_choice"
+
     featured = db.Column(db.Boolean(), default=False)
     editors_notes = db.Column(db.Unicode(500), nullable=True) # 500 char limit as defined in spec
     author = db.Column(None, db.ForeignKey("users.id"))
