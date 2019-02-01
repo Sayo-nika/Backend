240a1dfcdd7704a395301c147948f11b57a3c003
Ovyerus <iamovyerus@gmail.com>
2019-02-01 21:57:58 +1100

    feat(routes/api): check if user is already verified

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,5 @@
 # External Libraries
-from quart import jsonify, request, abort, Response
+from quart import jsonify, request, abort
 
 # Sayonika Internals
 from framework.authentication import Authenticator
@@ -48,21 +48,21 @@ class Userland(RouteCog):
         token = request.args.get("token")
 
         if token is None:
-            abort(400, "missing required parameter 'token'.")
+            abort(401, "No token provided")
 
-        parsed_token = jwt_service.verify_token(token, True)
+        parsed_token = await jwt_service.verify_email_token(token, True)
 
         if parsed_token is False:
-            abort(403, "Incorrect token.")
+            abort(400, "Invalid token")
 
-        user = User.get(parsed_token.id)
+        user = await User.get(parsed_token["id"])
 
-        if user is None:
-            abort(410, "Valid token, but user to validate does not exist.")
+        if user.email_verified:
+            return jsonify("Email already verified")
 
         await user.update(email_verified=True).apply()
 
-        return Response(response="OK.", status=200, content_type="text/plain")
+        return jsonify("Email verified")
 
 
 def setup(core: Sayonika):
