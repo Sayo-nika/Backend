53e9e41ab83cced215499db414949dd6484a3f4c
Ovyerus <iamovyerus@gmail.com>
2019-02-02 12:53:45 +1100

    feat(models/base): more indepth typing

--- framework/models/base.py
+++ framework/models/base.py
@@ -2,8 +2,10 @@
 from typing import List, Union
 
 # External Libraries
+from gino import Gino
 from simpleflake import simpleflake
 from sqlalchemy import or_, func
+from sqlalchemy.orm import Query
 
 # Sayonika Internals
 from framework.objects import db
@@ -13,11 +15,11 @@ class Base:
     id = db.Column(db.Unicode(), primary_key=True, default=lambda: str(simpleflake()))
 
     @classmethod
-    async def exists(cls: db.Model, id: str):
-        return not not (await cls.select('id').where(cls.id == id).gino.scalar())
+    async def exists(cls: db.Model, id: str) -> bool:
+        return bool(await cls.select('id').where(cls.id == id).gino.scalar())
 
     @classmethod
-    def get_any(cls: db.Model, insensitive: Union[bool, List[str]] = False, **kwargs):
+    def get_any(cls: db.Model, insensitive: Union[bool, List[str]] = False, **kwargs) -> Gino:
         if not len(kwargs):
             raise ValueError('No kwargs provided')
 
@@ -37,7 +39,7 @@ class Base:
         return cls.query.where(or_(*queries)).gino
 
     @classmethod
-    def paginate(cls: db.Model, page: int, limit: int = 50):
+    def paginate(cls: db.Model, page: int, limit: int = 50) -> Query:
         page = page - 1 if page > 0 else 0
 
         return cls.query.limit(limit).offset(page * limit)
