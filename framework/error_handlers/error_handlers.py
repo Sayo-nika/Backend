a2f187cd01b727626f6c1c666909a178bd169bba
Ovyerus <iamovyerus@gmail.com>
2019-03-09 17:31:50 +1100

    feat: custom 500 handler

--- framework/error_handlers/error_handlers.py
+++ framework/error_handlers/error_handlers.py
@@ -1,13 +1,19 @@
 # Stdlib
 from asyncio import Future
+import base64
+from datetime import datetime
 from functools import partial
+import json
+from traceback import format_exception
 
 # External Libraries
-from quart import Response
+from Cryptodome.Cipher import AES
+from quart import Response, request
 from quart.exceptions import WERKZEUG_EXCEPTION_CODES as exception_codes
 
 # Sayonika Internals
 from framework.error_handlers.error_common import error_handler
+from framework.jsonutils import DatetimeJSONEncoder
 
 
 def make_auto_future(val):
@@ -28,6 +34,46 @@ exception_handlers = {
     for code in exception_codes
 }
 
+# Custom 429 message
 exception_handlers[429] = error_handler(
     lambda err: make_auto_future(Response(f"Ratelimit for this endpoint: {err.description}", 429))
 )
+
+
+@error_handler
+async def handle_500(err):
+    from framework.objects import SETTINGS, jwt_service
+
+    tb = ''.join(
+        format_exception(type(err), err, err.__traceback__)
+    )
+    token = request.headers.get("Authorization", request.cookies.get("token"))
+    parsed = await jwt_service.verify_login_token(token, True)
+
+    if not parsed:
+        meta = {}
+    else:
+        meta = {
+            "id": parsed["id"]
+        }
+
+    meta["time"] = datetime.utcnow()
+
+    # Send traceback and metadata in base64 and encrypted.
+    data = json.dumps({
+        "tb": tb,
+        "d": meta
+    }, cls=DatetimeJSONEncoder).encode()
+
+    c = AES.new(SETTINGS["AES_KEY"], AES.MODE_CTR)
+    digest = base64.encodebytes(c.encrypt(data))
+    nonce = base64.b64encode(c.nonce)
+
+    res = nonce + b"." + digest
+
+    # TODO: Sentry error reporting integration
+
+    return Response(res.decode(), 500)
+
+
+exception_handlers[500] = handle_500
--- framework/objects.py
+++ framework/objects.py
@@ -30,6 +30,7 @@ SETTINGS = {
     "DB_PASS": "sayonika",
     "DB_NAME": "sayonika",
     "JWT_SECRET": "testing123",
+    "AES_KEY": "this is a  pretty long key oh no",
     "REDIS_URL": "redis://localhost:6379/0",
     "EMAIL_BASE": "http://localhost:4444"
 }
@@ -38,6 +39,12 @@ SETTINGS.update({
     k[9:]: v for k, v in os.environ.items()
     if k.startswith("SAYONIKA_")
 })
+
+if len(SETTINGS["AES_KEY"]) != 32:
+    # Early catch for wrong length key
+    raise ValueError("SAYONIKA_AES_KEY must be a 32 bit key.")
+else:
+    SETTINGS["AES_KEY"] = SETTINGS["AES_KEY"].encode()
 
 logger = logging.getLogger("Sayonika")
 sayonika_instance = Sayonika()
