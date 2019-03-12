72dd9c35f062fd71b284135b8f45e258d23d161d
Ovyerus <iamovyerus@gmail.com>
2019-03-12 16:53:48 +1100

    feat: base alembic environment

---
+++ env.py
@@ -0,0 +1,78 @@
+# Stdlib
+from logging.config import fileConfig
+import os
+import sys
+
+# External Libraries
+from alembic import context
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+
+sys.path.append(os.getcwd())
+
+# Sayonika Internals
+from framework.db import db as target_metadata  # isort:skip
+from framework.objects import SETTINGS  # isort:skip
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def get_url():
+    return URL("asyncpg", username=SETTINGS["DB_NAME"], password=SETTINGS["DB_PASS"], host=SETTINGS["DB_HOST"],
+               port=SETTINGS["DB_PORT"], database=SETTINGS["DB_NAME"])
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = get_url()
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(get_url())
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
