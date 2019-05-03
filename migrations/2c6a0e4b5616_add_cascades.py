f9c0888e951351f73edb3c8521b21686c7be3f39
Ovyerus <iamovyerus@gmail.com>
2019-05-03 17:33:45 +1000

    refactor(migrations): automate drop + create cascades

--- migrations/2c6a0e4b5616_add_cascades.py
+++ migrations/2c6a0e4b5616_add_cascades.py
@@ -6,6 +6,8 @@ Revision ID: 2c6a0e4b5616
 Revises: 3007e0d40f26
 Create Date: 2019-05-02 10:57:00.579594+00:00
 """
+import re
+
 # External Libraries
 from alembic import op
 import sqlalchemy as sa
@@ -17,72 +19,46 @@ down_revision = '3007e0d40f26'
 branch_labels = None
 depends_on = None
 
+constraint_re = re.compile(
+    r"^fk_"  # Start tag
+    r"(.*)_"  # Table
+    r"((?:author|user|mod|review)(?:_id)?)_"  # Column
+    r"(.*)$"  # Foreign table
+)
+
+constraints = [
+    "fk_connection_user_user",
+    "fk_editors_choice_author_id_user",
+    "fk_editors_choice_mod_id_mod",
+    "fk_media_mod_id_mod",
+    "fk_mod_playtester_mod_id_mod",
+    "fk_mod_playtester_user_id_user",
+    "fk_report_author_id_user",
+    "fk_report_mod_id_mod",
+    "fk_review_author_id_user",
+    "fk_review_mod_id_mod",
+    "fk_review_reaction_review_id_review",
+    "fk_review_reaction_user_id_user",
+    "fk_user_favorite_user_id_user",
+    "fk_user_favorite_mod_id_mod",
+    "fk_user_mod_user_id_user",
+    "fk_user_mod_mod_id_mod"
+]
+
 
 def upgrade():
-    op.drop_constraint('fk_connection_user_user', 'connection', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_connection_user_user'), 'connection', 'user', ['user'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_editors_choice_author_id_user', 'editors_choice', type_='foreignkey')
-    op.drop_constraint('fk_editors_choice_mod_id_mod', 'editors_choice', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_editors_choice_mod_id_mod'), 'editors_choice', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_editors_choice_author_id_user'), 'editors_choice', 'user', ['author_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_media_mod_id_mod', 'media', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_media_mod_id_mod'), 'media', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_mod_playtester_mod_id_mod', 'mod_playtester', type_='foreignkey')
-    op.drop_constraint('fk_mod_playtester_user_id_user', 'mod_playtester', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_mod_playtester_mod_id_mod'), 'mod_playtester', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_mod_playtester_user_id_user'), 'mod_playtester', 'user', ['user_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_report_author_id_user', 'report', type_='foreignkey')
-    op.drop_constraint('fk_report_mod_id_mod', 'report', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_report_mod_id_mod'), 'report', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_report_author_id_user'), 'report', 'user', ['author_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_review_author_id_user', 'review', type_='foreignkey')
-    op.drop_constraint('fk_review_mod_id_mod', 'review', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_review_mod_id_mod'), 'review', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_review_author_id_user'), 'review', 'user', ['author_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_review_reaction_review_id_review', 'review_reaction', type_='foreignkey')
-    op.drop_constraint('fk_review_reaction_user_id_user', 'review_reaction', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_review_reaction_review_id_review'), 'review_reaction', 'review', ['review_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_review_reaction_user_id_user'), 'review_reaction', 'user', ['user_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_user_favorite_user_id_user', 'user_favorite', type_='foreignkey')
-    op.drop_constraint('fk_user_favorite_mod_id_mod', 'user_favorite', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_user_favorite_user_id_user'), 'user_favorite', 'user', ['user_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_user_favorite_mod_id_mod'), 'user_favorite', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
-    op.drop_constraint('fk_user_mod_user_id_user', 'user_mod', type_='foreignkey')
-    op.drop_constraint('fk_user_mod_mod_id_mod', 'user_mod', type_='foreignkey')
-    op.create_foreign_key(op.f('fk_user_mod_user_id_user'), 'user_mod', 'user', ['user_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key(op.f('fk_user_mod_mod_id_mod'), 'user_mod', 'mod', ['mod_id'], ['id'], ondelete='CASCADE')
+    for constraint in constraints:
+        # Get all parts of constraint (table[_table part 2]_column[_column part 2]_foreign_table)
+        table, column, foreign = constraint_re.match(constraint).groups()
+
+        op.drop_constraint(constraint, table, type_="foreignkey")
+        op.create_foreign_key(op.f(constraint), table, foreign, [column], ['id'], ondelete='CASCADE')
 
 
 def downgrade():
-    op.drop_constraint(op.f('fk_user_mod_mod_id_mod'), 'user_mod', type_='foreignkey')
-    op.drop_constraint(op.f('fk_user_mod_user_id_user'), 'user_mod', type_='foreignkey')
-    op.create_foreign_key('fk_user_mod_mod_id_mod', 'user_mod', 'mod', ['mod_id'], ['id'])
-    op.create_foreign_key('fk_user_mod_user_id_user', 'user_mod', 'user', ['user_id'], ['id'])
-    op.drop_constraint(op.f('fk_user_favorite_mod_id_mod'), 'user_favorite', type_='foreignkey')
-    op.drop_constraint(op.f('fk_user_favorite_user_id_user'), 'user_favorite', type_='foreignkey')
-    op.create_foreign_key('fk_user_favorite_mod_id_mod', 'user_favorite', 'mod', ['mod_id'], ['id'])
-    op.create_foreign_key('fk_user_favorite_user_id_user', 'user_favorite', 'user', ['user_id'], ['id'])
-    op.drop_constraint(op.f('fk_review_reaction_user_id_user'), 'review_reaction', type_='foreignkey')
-    op.drop_constraint(op.f('fk_review_reaction_review_id_review'), 'review_reaction', type_='foreignkey')
-    op.create_foreign_key('fk_review_reaction_user_id_user', 'review_reaction', 'user', ['user_id'], ['id'])
-    op.create_foreign_key('fk_review_reaction_review_id_review', 'review_reaction', 'review', ['review_id'], ['id'])
-    op.drop_constraint(op.f('fk_review_author_id_user'), 'review', type_='foreignkey')
-    op.drop_constraint(op.f('fk_review_mod_id_mod'), 'review', type_='foreignkey')
-    op.create_foreign_key('fk_review_mod_id_mod', 'review', 'mod', ['mod_id'], ['id'])
-    op.create_foreign_key('fk_review_author_id_user', 'review', 'user', ['author_id'], ['id'])
-    op.drop_constraint(op.f('fk_report_author_id_user'), 'report', type_='foreignkey')
-    op.drop_constraint(op.f('fk_report_mod_id_mod'), 'report', type_='foreignkey')
-    op.create_foreign_key('fk_report_mod_id_mod', 'report', 'mod', ['mod_id'], ['id'])
-    op.create_foreign_key('fk_report_author_id_user', 'report', 'user', ['author_id'], ['id'])
-    op.drop_constraint(op.f('fk_mod_playtester_user_id_user'), 'mod_playtester', type_='foreignkey')
-    op.drop_constraint(op.f('fk_mod_playtester_mod_id_mod'), 'mod_playtester', type_='foreignkey')
-    op.create_foreign_key('fk_mod_playtester_user_id_user', 'mod_playtester', 'user', ['user_id'], ['id'])
-    op.create_foreign_key('fk_mod_playtester_mod_id_mod', 'mod_playtester', 'mod', ['mod_id'], ['id'])
-    op.drop_constraint(op.f('fk_media_mod_id_mod'), 'media', type_='foreignkey')
-    op.create_foreign_key('fk_media_mod_id_mod', 'media', 'mod', ['mod_id'], ['id'])
-    op.drop_constraint(op.f('fk_editors_choice_author_id_user'), 'editors_choice', type_='foreignkey')
-    op.drop_constraint(op.f('fk_editors_choice_mod_id_mod'), 'editors_choice', type_='foreignkey')
-    op.create_foreign_key('fk_editors_choice_mod_id_mod', 'editors_choice', 'mod', ['mod_id'], ['id'])
-    op.create_foreign_key('fk_editors_choice_author_id_user', 'editors_choice', 'user', ['author_id'], ['id'])
-    op.drop_constraint(op.f('fk_connection_user_user'), 'connection', type_='foreignkey')
-    op.create_foreign_key('fk_connection_user_user', 'connection', 'user', ['user'], ['id'])
+    for constraint in constraints:
+        # Get all parts of constraint (table[_table part 2]_column[_column part 2]_foreign_table)
+        table, column, foreign = constraint_re.match(constraint).groups()
+
+        op.drop_constraint(op.f(constraint), table, type_="foreignkey")
+        op.create_foreign_key(constraint, table, foreign, [column], ['id'])
