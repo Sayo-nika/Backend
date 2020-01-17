727d9e7a6fb7b9eb50888d0735e6442caaa67678
Ovyerus <iamovyerus@gmail.com>
2020-01-17 16:24:05 +1100

    [models/review] add setters for reactions

--- framework/models/review.py
+++ framework/models/review.py
@@ -14,6 +14,44 @@ class Review(db.Model, Base):
     mod_id = db.Column(None, db.ForeignKey("mod.id", ondelete="CASCADE"))
     author_id = db.Column(None, db.ForeignKey("user.id", ondelete="CASCADE"))
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._upvotes = []
+        self._downvotes = []
+        self._funnys = []
+
+    @property
+    def upvotes(self):
+        return self._upvotes
+
+    @property
+    def downvotes(self):
+        return self._downvotes
+
+    @property
+    def funnys(self):
+        return self._funnys
+
+    @upvotes.setter
+    def upvotes(self, value: "ReviewReaction"):
+        self._upvotes.append(value)
+
+    @downvotes.setter
+    def downvotes(self, value: "ReviewReaction"):
+        self._downvotes.append(value)
+
+    @funnys.setter
+    def funnys(self, value: "ReviewReaction"):
+        self._funnys.append(value)
+
+    def to_dict(self):
+        return {
+            **{k: v for k, v in super().to_dict},
+            "upvotes": self._upvotes,
+            "downvotes": self._downvotes,
+            "funnys": self._funnys,
+        }
 
 class ReviewReaction(db.Model, Base):
     __tablename__ = "review_reaction"
